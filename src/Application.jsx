7279d064a96ad459efd95bce1340aa1bc6bd62ea
Mike Randrup <Mike.Randrup@match.com>
2016-07-12 17:43:17 -0500

    Added additional mock data to app

--- src/Application.jsx
+++ src/Application.jsx
@@ -5,7 +5,22 @@ import SettingsView from './SettingsView';
 
 import appViews from './constants/AppViews';
 
-const CURRENT_VIEW = appViews.MapView;
+const CURRENT_VIEW = appViews.TemperatureView;
+
+const viewData = {
+  currentTemp: 83,
+  targetTemp: 72,
+
+  currentLocation: 'Dallas TX USA',
+  targetLocation: 'San Diego CA USA',
+
+  // maybe do current lat/lon & target lat/lon
+
+  targetDistance: '453 miles',
+  arrivalWait: '0 hours 13 minutes 42 seconds',
+
+  travelSpeed: '371 mph'
+};
 
 export default class Application extends Component {
   render() {
@@ -27,7 +42,7 @@ export default class Application extends Component {
     }
 
     return (
-      <ViewComponent />
+      <ViewComponent {...viewData} />
     );
   }
 }
