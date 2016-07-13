5afb55f9974ac35074746545088d4b1aab178c89
Mike Randrup <Mike.Randrup@match.com>
2016-07-13 17:30:20 -0500

    #4 Wired GoogleStaticMap component to actual Google API

--- src/GoogleStaticMap.jsx
+++ src/GoogleStaticMap.jsx
@@ -1,5 +1,10 @@
 import React, { Component, PropTypes } from 'react';
 import styles from './styles/googlemap.scss';
+import googleApiKey from './constants/GoogleMap.secret';
+
+const schemeAndHost = 'https://maps.googleapis.com/maps/api/staticmap';
+const zoomLevel = 2;
+
 
 export default class GoogleStaticMap extends Component {
 
@@ -23,8 +28,24 @@ export default class GoogleStaticMap extends Component {
     };
   }
 
+  static get getDefaultProps() {
+    return {
+      latitude: 20,
+      longitude: -20
+    };
+  }
+
   buildImageUrl(width, height) {
-    return `https://placekitten.com/g/${width}/${height}`;
+    const dimensions = `${width}x${height}`;
+    const center = `${this.props.latitude},${this.props.longitude}`;
+
+    return [
+      schemeAndHost,
+      '?', 'center=', center,
+      '&', 'zoom=', zoomLevel,
+      '&', 'size=', dimensions,
+      '&', 'key=', googleApiKey
+    ].join('');
   }
 
   render() {
