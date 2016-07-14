269642e8c812c5999b773d85c89bbdf650a2cc72
Mike Randrup <Mike.Randrup@match.com>
2016-07-14 14:48:02 -0500

    #13 Created actions for target temperature change

---
+++ src/actions/TemperatureActions.jsx
@@ -0,0 +1,20 @@
+import {TargetTempIncreased, TargetTempDecreased} from '../constants/ActionNames';
+import eventemitter from 'events';
+
+const TemperatureActions = {
+
+  targetTempIncreased: (context) => {
+    context.dispatch(
+      TargetTempIncreased
+    );
+  },
+
+  targetTempDecreased: (context) => {
+    context.dispatch(
+      TargetTempDecreased
+    );
+  }
+
+};
+
+export default AppViewActions;
--- src/constants/ActionNames.js
+++ src/constants/ActionNames.js
@@ -1,1 +1,4 @@
 export const ChangeView = '@@CHANGE_VIEW';
+
+export const TargetTempIncreased = '@@TARGET_TEMPERATURE_INCREASED';
+export const TargetTempDecreased = '@@TARGET_TEMPERATURE_DECREASED';
